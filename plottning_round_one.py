5984a15640092acfd829d2991d16153f461d0bfa
Lucy Chong <lchong@caltech.edu>
2016-09-13 13:36:29 -0700

    Comparing histograms

--- plottning_round_one.py
+++ plottning_round_one.py
@@ -11,10 +11,17 @@ xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
 
 # Make the bin boundaries.
-bins = np.arange(1700, 2500, 50)
+low_min = np.min(xa_low)
+low_max = np.max(xa_low)
+high_min = np.min(xa_high)
+high_max = np.max(xa_high)
+global_min = np.min([low_min, high_min])
+global_max = np.max([low_max, high_max])
+
+bins = np.arange(global_min-50, global_max+50, 50)
 
 # Plot data as a histogram.
-_= plt.hist(xa_low, bins=bins)
+_= plt.hist((xa_low, xa_high), bins=bins)
 plt.xlabel('Cross-sectional area(µm$^2$)')
 plt.ylabel('Count')
 plt.show()
